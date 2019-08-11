bf194122fcad27c5474447d5c328cd6c1b542622
Zack Stickles <zstickles@gmail.com>
2019-08-11 12:42:33 -0500

    Initial decision tree for testing

---
+++ src/data.js
@@ -0,0 +1,47 @@
+// NOTE: this is dummy information, awaiting final copy
+export default [
+  {
+    id: 1,
+    text: 'You\'re having a great time after the wedding. How would you like to keep this party going?',
+    options: [
+      { text: 'grab some food', value: 2 },
+      { text: 'more drinks', value: 3 }
+    ]
+  },
+  {
+    id: 2,
+    text: 'How hungry are you feeling?',
+    options: [
+      { text: 'super hungry', value: 4 },
+      { text: 'eh, a nibble is fine', value: 5 }
+    ]
+  },
+  {
+    id: 3,
+    text: 'You have a thirst that needs to be quenched. How fancy do you feel?',
+    options: [
+      { text: 'pinkys out', value: 6 },
+      { text: 'keep it simple', value : 7 }
+    ]
+  },
+  {
+    id: 4,
+    text: 'Check out the sink',
+    value: 'location'
+  },
+  {
+    id: 5,
+    text: 'The med has great tapas!',
+    value: 'location'
+  },
+  {
+    id: 6,
+    text: 'license no. 1 will make you a fancy cocktail',
+    value: 'location'
+  },
+  {
+    id: 7,
+    text: 'grab a pint (or stein) at the beir garden',
+    value: 'location'
+  }
+];
