fd975a848292e6ff124353c4b3c1c0672ebce4e0
Zack Stickles <zstickles@gmail.com>
2019-08-12 17:51:49 -0500

    Added very basic type system

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Line = ({ text }) => (
   <p>{text}</p>
 );
+
+Line.propTypes = {
+  text: PropTypes.string
+};
 
 export default Line;
