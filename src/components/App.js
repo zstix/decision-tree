7ae5437dc2f888ebfbe5113c187fa6e15d73b658
Zack Stickles <zstickles@gmail.com>
2019-08-12 18:02:54 -0500

    Moved global styles out of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
-import { createGlobalStyle } from 'styled-components';
 import { splitText } from '../utils';
 
+import GlobalStyle from '../styles/global';
 import Line from './Line';
 import paper from '../cream-paper.png';
 import data from '../data';
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Alegreya&display=swap');
-
-  body {
-    background-color: #f5efc9;
-    background-image: url(${paper});
-    padding-top: 2em;
-    text-align: center;
-  }
-`;
 
 const App = () => {
   const [nodeIndex, setNodeIndex] = useState(1);
---
+++ src/styles/global.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+import paper from '../cream-paper.png';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Alegreya&display=swap');
+
+  body {
+    background-color: #f5efc9;
+    background-image: url(${paper});
+    padding-top: 2em;
+    text-align: center;
+  }
+`;
